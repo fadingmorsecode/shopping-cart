d048d0b1a3d9c5ffaba425f9f4b55e070af695a6
Drake Williamson <williamson.drake@outlook.com>
2024-07-16 10:02:43 -0700

    finish styling and add empty cart text

--- src/components/shoppingcart/shoppingcart.jsx
+++ src/components/shoppingcart/shoppingcart.jsx
@@ -1,20 +1,27 @@
 import { useOutletContext } from 'react-router-dom';
+import styles from './shoppingcart.module.css';
 
 function Item({ data }) {
   return (
-    <li>
-      <div>
-        <img src={data.image} />
-        <p>{data.title}</p>
+    <li className={styles.cartListItem}>
+      <div className={styles.cartImageTextGroup}>
+        <img src={data.image} className={styles.cartListImage} />
+        <p className={styles.cartListTitle}>{data.title}</p>
       </div>
-      <div>
-        <p data-testid='product-quantity'>
+      <div className={styles.cartListItemInfo}>
+        <p data-testid='product-quantity' className={styles.cartListQuantity}>
           {data.quantity < 2
             ? data.quantity + ' item'
             : data.quantity + ' items'}
         </p>
-        <p>${(data.price * data.quantity).toFixed(2)}</p>
-        {data.quantity > 1 && <p>{'each $' + data.price}</p>}
+        <p className={styles.cartListPrice}>
+          ${(data.price * data.quantity).toFixed(2)}
+        </p>
+        {data.quantity > 1 && (
+          <p className={styles.cartListIndividualPrice}>
+            {'each $' + data.price}
+          </p>
+        )}
       </div>
     </li>
   );
@@ -38,30 +45,39 @@ export default function ShoppingCart() {
   const cartCount = getCartCount();
 
   return (
-    <>
-      <h2>Cart</h2>
-      <div>
-        <p>${getCartSubtotal()} subtotal</p>
-        <p data-testid='total-cart-count'>
+    <div className={styles.cartContainer}>
+      <h2 className={styles.cartHeading}>Cart</h2>
+      <div className={styles.cartInfo}>
+        <p className={styles.cartSubtotal}>${getCartSubtotal()} subtotal</p>
+        <span>•</span>
+        <p className={styles.cartCount} data-testid='total-cart-count'>
           {cartCount > 2 || cartCount < 1
             ? cartCount + ' items'
             : cartCount + ' item'}
         </p>
       </div>
-      <main>
-        <ul>
+      {cart.length < 1 && (
+        <h2 className={styles.emptyCartText}>Your cart is empty</h2>
+      )}
+      <main className={styles.cartMainContent}>
+        <ul className={styles.cartList}>
           {cart.map((item) => (
             <Item key={item.id} data={item} />
           ))}
         </ul>
         {cart.length > 0 && (
-          <div>
-            <p>Order summary</p>
-            <p>${getCartSubtotal()} subtotal</p>
-            <button onClick={handlePlacedOrder}>Place Order</button>
+          <div className={styles.placeOrderSection}>
+            <p className={styles.orderSummary}>Order summary</p>
+            <p className={styles.cartSubtotal}>${getCartSubtotal()} subtotal</p>
+            <button
+              onClick={handlePlacedOrder}
+              className={styles.placeOrderBtn}
+            >
+              Place Order
+            </button>
           </div>
         )}
       </main>
-    </>
+    </div>
   );
 }
