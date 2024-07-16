ed2a1ab965c9e15aa5063dccb077e8453d829a2d
Drake Williamson <williamson.drake@outlook.com>
2024-07-16 05:36:31 -0700

    create mobile styling for header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,16 +1,40 @@
 import { Link } from 'react-router-dom';
+import styles from './header.module.css';
 
 export default function Header({ cartCount }) {
   return (
-    <header data-testid='header-component'>
-      <img src='/aberdeen-logo.svg' alt='Aberdeen cobweb logo' />
-      <h1>Aberdeen</h1>
-      <Link to='/'>Home</Link>
-      <Link to='/shop'>Shop</Link>
-      <Link to='/cart' data-testid='cart-button'>
-        <img src='/shopping-cart.svg' alt='Shopping cart icon' />
-      </Link>
-      <p data-testid='cart-count'>{cartCount > 0 && cartCount}</p>
+    <header data-testid='header-component' className={styles.header}>
+      <div className={styles.logoTextGroup}>
+        <img
+          src='/aberdeen-logo.svg'
+          alt='Aberdeen cobweb logo'
+          className={styles.logoImg}
+        />
+        <h1 className={styles.headerCompanyText}>Aberdeen</h1>
+      </div>
+      <div className={styles.linkCartGroup}>
+        <Link to='/' className={styles.textLinks}>
+          Home
+        </Link>
+        <Link to='/shop' className={styles.textLinks}>
+          Shop
+        </Link>
+        <div className={styles.cartGroup}>
+          <Link to='/cart' data-testid='cart-button'>
+            <img
+              src='/shopping-cart.svg'
+              alt='Shopping cart icon'
+              className={styles.cartImg}
+            />
+          </Link>
+          <p
+            data-testid='cart-count'
+            className={cartCount > 0 && styles.cartCount}
+          >
+            {cartCount > 0 ? (cartCount > 99 ? '99+' : cartCount) : null}
+          </p>
+        </div>
+      </div>
     </header>
   );
 }
